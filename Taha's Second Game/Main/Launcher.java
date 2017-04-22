bcd1dab067049e0a8cfd5e165d3ff3bc0bf68659
supertaha <supertaha@gmail.com>
2017-04-22 14:07:41 -0400

    Beginning of game

---
+++ Taha's Second Game/Main/Launcher.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Display.Display;
+
+public class Launcher {
+
+	public static void main (String [] args){
+		Game game = new Game("Taha's Game",400,400);
+		game.start();
+		
+	}
+	
+}
